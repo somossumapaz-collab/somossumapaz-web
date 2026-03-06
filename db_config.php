ecb7c9515ea023ecf8c5f64ad27932a8c5f98921
Andy Soto <sumapazsomos@gmail.com>
2026-03-05 22:54:08 -0500

    creación de estrategia 1

--- db_config.php
+++ db_config.php
@@ -1,10 +1,4 @@
 <?php
-
-$host = "localhost";
-$user = "somossum_admin";
-$password = "somossumapaz2026*";
-$database = "somossum_general";
-
 function get_db_connection()
 {
     static $conn = null;
@@ -13,25 +7,21 @@ function get_db_connection()
         return $conn;
     }
 
-    global $host, $user, $password, $database;
+    $host = "localhost";
+    $user = "somossum_admin";
+    $password = "somossumapaz2026*";
+    $database = "somossum_general";
 
-    try {
+    mysqli_report(MYSQLI_REPORT_OFF);
 
-        mysqli_report(MYSQLI_REPORT_OFF);
+    $conn = new mysqli($host, $user, $password, $database);
 
-        $conn = new mysqli($host, $user, $password, $database);
-
-        if ($conn->connect_error) {
-            die("Error de conexión: " . $conn->connect_error);
-        }
-
-        $conn->set_charset("utf8mb4");
-
-        return $conn;
-
-    } catch (Exception $e) {
-
-        die("Error de conexión a la base de datos.");
-
+    if ($conn->connect_error) {
+        die("Error de conexión: " . $conn->connect_error);
     }
+
+    $conn->set_charset("utf8mb4");
+
+    return $conn;
 }
+?>
