e422b13becd823c00f62c38ddde35061415d2c2a
Andy Soto <sumapazsomos@gmail.com>
2026-03-05 22:46:06 -0500

    creación de estrategia 1

--- api/login.php
+++ api/login.php
@@ -1,4 +1,9 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 // api/login.php
 session_start();
 require_once '../database_functions.php';
